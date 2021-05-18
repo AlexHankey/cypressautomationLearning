b6eddc4397206008d59ca8d4434d8fcf882e3e5c
Alex <alex.hankey@myclevergroup.com>
2021-05-18 13:13:43 +0100

    Removed spec file & updated test

--- cypress/integration/examples/firsttest.js
+++ cypress/integration/examples/firsttest.js
@@ -1,11 +1,5 @@
 describe('My first test', function()
 {
-    it('Verify Title of the page-positive', function()
-    {
-        cy.visit('https://demo.nopcommerce.com/')
-        cy.title().should('eq','nopCommerce demo store')
-    })
-
     it('Verify Title of the page-negative', function()
     {
         cy.visit('https://demo.nopcommerce.com/')
